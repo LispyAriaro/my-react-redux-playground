3681a3a4446bad92a33a57bacf1dedcbe56c5c27
Efe Ariaroo <efeariaroo@gmail.com>
2018-03-26 02:03:04 +0100

    made actions modify state

--- src/containers/Home.js
+++ src/containers/Home.js
@@ -10,24 +10,53 @@ import {
 } from '../reducers/counter'
 
 
-class Home extends Component {
-  render() {
-    return (
-        <div>
-            No change to give a customer? We help record how much change is owed.
-            <div className="">
-                <Link to="/mc-admin/businesslogin">Log in</Link>
-                <Link to="/mc-admin/businessSignup">Sign up</Link>
-            </div>
-        </div>
-    );
-  }
-}
+// class Home extends Component {
+//   render() {
+//     return (
+//         <div>
+//             No change to give a customer? We help record how much change is owed.
+//             <div className="">
+//                 <Link to="/mc-admin/businesslogin">Log in</Link>
+//                 <Link to="/mc-admin/businessSignup">Sign up</Link>
+//             </div>
+
+//             <p>Count: {props.count}</p>
+
+//             <p>
+//               <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+//               <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+//             </p>
+//         </div>
+//     );
+//   }
+// }
+
+const Home = props => (
+  <div>
+    No change to give a customer? We help record how much change is owed.
+    <div className="">
+      <Link to="/mc-admin/businesslogin">Log in</Link>
+      <Link to="/mc-admin/businessSignup">Sign up</Link>
+    </div>
+
+    <p>Count: {props.count}</p>
+
+    <p>
+      <button onClick={props.increment} disabled={props.isIncrementing}>Increment</button>
+      <button onClick={props.incrementAsync} disabled={props.isIncrementing}>Increment Async</button>
+    </p>
+
+    <p>
+      <button onClick={props.decrement} disabled={props.isDecrementing}>Decrementing</button>
+      <button onClick={props.decrementAsync} disabled={props.isDecrementing}>Decrement Async</button>
+    </p>
+  </div>
+)
 
 // export default Home;
 
 const mapStateToProps = state => ({
-  count: state.counter.count,
+  count: state.counter.get('value'),
   isIncrementing: state.counter.isIncrementing,
   isDecrementing: state.counter.isDecrementing
 })
